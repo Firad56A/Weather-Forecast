bdf2c03befbc0ea5827a1e3c39a4cfd8f96ad180
Firad <firad.aslanov@edu.rtu.lv>
2024-02-21 17:32:13 -0800

    Day 33: Setting up backend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import plotly.express as px
-
+import backend import get_data
 # st.set_page_config(layout="wide")
 
 st.title("Weather Forecast for the Next days")
@@ -13,13 +13,7 @@ option = st.selectbox("Select data to view",
 
 st.subheader(f"{option} for the next {days} days in {place}")
 
-
-def get_data(days):
-    dates = ["2022-25-10", "2022-26-10", "2022-27-10"]
-    temperatures = [10, 11, 15]
-    temperatures = [days * i for i in temperatures]
-    return dates, temperatures
-
+data = get_data(place, days, option)
 
 d, t = get_data(days)
 
